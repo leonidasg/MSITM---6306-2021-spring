8d92b51fee8eac95ba3ccef8266535e9c6d93d6e
leonidasg <47127386+leonidasg@users.noreply.github.com>
2021-03-20 15:56:47 -0500

    Update assignment 1 - use pandas do DV.py

--- assignment 1 - use pandas do DV.py
+++ assignment 1 - use pandas do DV.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+'''
+The rising average temperature of Earth's climate system, called global warming, is driving changes in rainfall patterns, extreme weather, arrival of seasons, and more. Collectively, global warming and its effects are known as climate change. While there have been prehistoric periods of global warming, observed changes since the mid-20th century have been unprecedented in rate and scale. So a dataset on the temperature of major cities of the world will help analyze the same. Also weather information is helpful for a lot of data science tasks like sales forecasting, logistics etc. The data is available for research and non-commercial purposes only.
+This data set, with a lot of data, is very comprehensive and very representative. i downloaded it from “Kaggle.com”. My target is use Machine Learning knowledge to observe changes in the annual average temperature of major cities
+And those day, I'm using PyCharm to practice Python, so i decide to us PyCharm to finish it.
+'''
 # In[1]:
 
 
